659873368b77cf2b3b03a78ed701b2a17b4878b1
김한주 <s24043@gsm.hs.kr>
2024-11-07 23:34:51 +0900

    add:: SecurityConfig.java

---
+++ src/main/java/org/example/googleoauth2/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package org.example.googleoauth2.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.googleoauth2.entity.MemberRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/oauth-login/admin").hasRole(MemberRole.ADMIN.name())
+                        .requestMatchers("/oauth-login/info").authenticated()
+                        .anyRequest().permitAll()
+                );
+
+        http
+                .formLogin((auth) -> auth.loginPage("/oauth-login/login")
+                        .loginProcessingUrl("/oauth-login/loginProc")
+                        .usernameParameter("loginId")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/oauth-login")
+                        .failureUrl("/oauth-login")
+                        .permitAll());
+
+        http
+                .oauth2Login((auth) -> auth.loginPage("/oauth-login/login")
+                        .defaultSuccessUrl("/oauth-login")
+                        .failureUrl("/oauth-login/login")
+                        .permitAll());
+
+        http
+                .logout((auth) -> auth
+                        .logoutUrl("/oauth-login/logout"));
+
+        http
+                .csrf((auth) -> auth.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+
+
+        return new BCryptPasswordEncoder();
+    }
+}
