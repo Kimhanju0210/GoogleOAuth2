31a6dd00f30369f03d74496992ca1f744db66ca1
김한주 <s24043@gsm.hs.kr>
2024-11-07 22:40:49 +0900

    add

---
+++ src/main/java/org/example/googleoauth2/GoogleOAuth2Application.java
@@ -0,0 +1,13 @@
+package org.example.googleoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleOAuth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleOAuth2Application.class, args);
+    }
+
+}
